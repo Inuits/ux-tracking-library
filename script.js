ef9d0be4b0605e241ea3c1d16235fca17522e0ba
Lucas Pauwels <pauwelslucas2@gmail.com>
2018-05-03 09:19:39 +0200

    Added action history untill an error comes up, an error comes up after 5 and 15 seconds

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-$(document).on('click', function(event){
-    let b = sldkfj;
+var actions = [];
+var value;
+
+$('body').on('click', function (event) {
+
+    if (event.target.nodeName === "INPUT") {
+        value = event.target.value;
+    } else {
+        value = event.target.innerText;
+    }
+
+    actions.push("id: "
+        + event.target.id + "\nclass: "
+        + event.target.className + "\nname: "
+        + event.target.name + "\nvalue: "
+        + value + "\ntimestamp: "
+        + new Date().getTime() + "\ntype:"
+        + event.target.nodeName + "\npath:"
+        + window.location.pathname + "\n\n");
 });
 
-window.onerror = function(message, source, lineno, colno, error){
+setTimeout(function waitaMinute() {
+    addlert("Dit is een error");
+}, 5000);
+
+setTimeout(function waitaMinute() {
+    addlert("Dit is een error");
+}, 15000);
+
+window.onerror = function (message, source, lineno, colno, error) {
+
+    console.log("Actions happened so far:\n" + actions);
+
     let re = /https?:\/\/([^\/]*)\/(.*)/;
 
     let data = {
         'error': message,
         'source': re.exec(source)[2],
         'position': lineno + ',' + colno,
-        'stack': error.stack
+        'stack': error.stack,
+        'actions': actions
     };
 
-    $.post('http://localhost:5000/error', data, function(resp){});
+    $.post('http://localhost:1234/error', data, function(resp){});
+
+    actions = [];
+    console.log("Actions after error:\n" + actions);
 };
